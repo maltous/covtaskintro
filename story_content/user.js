eecebf936559012f5fa9b0e13a264f4ae53d7aaa
nsddksmc <40565225+nsddksmc@users.noreply.github.com>
2020-05-17 14:32:08 +0300

    covtaskintro

---
+++ story_content/user.js
@@ -0,0 +1,47 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6KzOlC5ByB3":
+        Script1();
+        break;
+      case "5ZvcVhkTxlp":
+        Script2();
+        break;
+      case "5jIKhvtaLK5":
+        Script3();
+        break;
+      case "5dBMS0617Nv":
+        Script4();
+        break;
+      case "6mLYXCEW4Qx":
+        Script5();
+        break;
+  }
+}
+
+function Script1()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script2()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script3()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script4()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script5()
+{
+  window.print();
+}
+
